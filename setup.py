8cf734dc0f8de645b205741833ec2ab490f484df
Oleg Lupats <oleglupats@gmail.com>
2021-11-20 12:11:04 +0200

    Release 0.0.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='xls_report',
-      version='0.0.5',
+      version='0.0.6',
       packages=['xls_report'],
       url='https://github.com/oleglpts/xls_report',
       license='MIT',
